879b99339e65682e33387bacdc57b4a27076e91e
soccho roy <socchoroy52@gmail.com>
2024-07-14 13:45:05 +0600

    Explicit Animation Done

--- resturent_app_android/lib/provider/meal_provider.dart
+++ resturent_app_android/lib/provider/meal_provider.dart
@@ -1,5 +1,5 @@
 import 'package:meals/data/dummy_data.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final mealProvider = Provider((ref) {
   return dummyMeals;
--- resturent_app_android/lib/screen/categories.dart
+++ resturent_app_android/lib/screen/categories.dart
@@ -5,14 +5,40 @@ import 'package:meals/screen/meals.dart';
 import 'package:meals/widgets/categories_grid_item.dart';
 import 'package:meals/model/category.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({required this.availableMeals, super.key});
 
   // final void Function(Meal meal) onToogleFavourite;
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+      upperBound: 1,
+      lowerBound: 0,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
   void _selectedScreen(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList(); //here dummyMeal
     Navigator.of(context).push(
@@ -27,24 +53,37 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: GridView(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final category in availableCategories)
+              CategoriesGridItem(
+                category: category,
+                onSelectedCategory: () {
+                  _selectedScreen(context, category);
+                },
+              )
+          ],
         ),
-        children: [
-          for (final category in availableCategories)
-            CategoriesGridItem(
-              category: category,
-              onSelectedCategory: () {
-                _selectedScreen(context, category);
-              },
-            )
-        ],
+      ),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: Offset(0, 0.3),
+          end: Offset(0, 0),
+        ).animate(CurvedAnimation(
+          parent: _animationController,
+          curve: Curves.easeInOut,
+        )),
+        child: child,
       ),
     );
   }
